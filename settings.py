ac472cda363f316705543ab32b2a3f36e0b4ea68
Tom Miller <cryptite@gmail.com>
2014-01-17 16:46:49 -0600

    proper settings, no local settings for web

--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@ from unipath import Path
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #djcelery.setup_loader()
 
-DEBUG = True
-TEMPLATE_DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = False
 
 ADMINS = (
     ('Tom Miller', 'cryptite@gmail.com'),
