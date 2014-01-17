b2625c9b6bdacb07759f896f601d5f462123957d
Tom Miller <cryptite@gmail.com>
2014-01-17 16:48:25 -0600

    wrong password

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'django', # Or path to database file if using sqlite3.
         'USER': 'django', # Not used with sqlite3.
-        'PASSWORD': 'chahPhe6', # Not used with sqlite3.
+        'PASSWORD': 'chahPHe6', # Not used with sqlite3.
         'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306', # Set to empty string for default. Not used with sqlite3.
     }
