7668de58c808f303b771f4b7979b34e74841f108
Tom Miller <cryptite@gmail.com>
2014-01-17 16:06:32 -0600

    stupid settings.py

--- settings.py
+++ settings.py
@@ -17,9 +17,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'loka', # Or path to database file if using sqlite3.
-        'USER': 'root', # Not used with sqlite3.
-        'PASSWORD': 'cally342', # Not used with sqlite3.
+        'NAME': 'django', # Or path to database file if using sqlite3.
+        'USER': 'django', # Not used with sqlite3.
+        'PASSWORD': 'chahPhe6', # Not used with sqlite3.
         'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306', # Set to empty string for default. Not used with sqlite3.
     }
