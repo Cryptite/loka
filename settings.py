3b37262b2aad93c186c2e9cafb284eed684a1e6b
cryptite <crypt342>
2014-07-18 16:37:03 -0500

    Alliance model, added level/alliance to towns

--- settings.py
+++ settings.py
@@ -5,10 +5,10 @@ from unipath import Path
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#djcelery.setup_loader()
+# djcelery.setup_loader()
 
 DEBUG = True
-TEMPLATE_DEBUG = True 
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['loka.minecraftarium.com']
 
@@ -209,6 +209,9 @@ THUMBNAIL_PROCESSORS = (
 
 IMAGE_CROPPING_SIZE_WARNING = True
 
+SOUTH_MIGRATION_MODULES = {
+    'easy_thumbnails': 'easy_thumbnails.south_migrations',
+}
 
 ###################
 # EMAIL SETTINGS #
